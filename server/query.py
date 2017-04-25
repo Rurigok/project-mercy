d3347c4bc5002f5728c35729f2f143d3a3bc1352
Edward Mondragon <insomnawolf@gmail.com>
2017-04-24 21:21:58 -0500

    Created a simple game mode, just to test the class functionality.

--- server/query.py
+++ server/query.py
@@ -1,32 +1,54 @@
+
+
 #Written by Edward Mondragon
 
 class Player(object):
-    health = 3
+    health = 5
     base_attack = 3
+
     def __init__(self):
         pass
+
 
 class Entity(object):
     name = ""
     description = ""
     attributes = []
-    def __init__(self, name, description):
+
+    def __init__(self, name, description, attribs):
         self.name = name
         self.description = description
+        self.attributes = attribs
+
 
 class Tile(object):
-    tileID = -1
-    tileDescription = ""
+    tile_id = -1
+    tile_description = ""
     entities = []
     neighboringIDs = []
-    def __init__(self, id, description):
-        self.tileID = id
-        self.tileDescription = description
+
+    def __init__(self, tile_id, description):
+        self.tile_id = tile_id
+        self.tile_description = description
+
+    def set_description(self, description):
+        self.tile_description = description
+
+    def get_description(self):
+        print(self.tile_description)
+
 
 class Map(object):
     tiles = []
-    def __init__(self):
-        tiles.append(Tile());
 
-def game(player, map):
+    def __init__(self):
+        self.tiles.append(Tile(0, "A room of index " + str(0)))
+
+
+def game(player, game_map):
+    print(game_map.tiles[0].get_description())
     return
+
+player1 = Player()
+my_map = Map()
+game(player1, my_map)
